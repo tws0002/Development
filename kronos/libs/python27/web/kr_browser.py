88317cd0998e2ed457143be37f86f67886e6f558
Masato Tsuzuki <msjbinc@gmail.com>
2018-09-05 16:48:53 +0900

    Update kr_browser.py

--- kronos/libs/python27/web/kr_browser.py
+++ kronos/libs/python27/web/kr_browser.py
@@ -3,8 +3,6 @@ import platform
 import subprocess
 import os
 
-def PageforProjects(url):
-    if platform.system() == 'Windows':
-       chrome_Dir = os.path.isdir("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
-       if chrome_Dir == 'True':
-          webbrowser.get(chrome_Dir).open_new_tab(url)
+def OpenBrowser(url):
+
+    webbrowser.open(url)
